b13760e1af63924dbe503e4360a1c56e376e1dc4
Kayla Hauge <kmhauge@ufl.edu>
2018-04-22 15:33:01 -0400

    alter content on events page

--- js/main.js
+++ js/main.js
@@ -8,6 +8,6 @@ $(document).ready(function () {
 var selected = document.getElementById('events');
 var newElement = document.createElement('section');
 
-newElement.innerHTML = '<h2>Bouquets with a Twist</h2><img src= "img/flowers.jpg" alt="flowers"><p>Like painting with a twist, but with flowers! Grab your friends and join us for a fun night of bouquet building. We will take you through the step by step process of building a bouquet all while enjoying drinks and small bites. We will use seasonal flowers to build a lovely bouquet.</p>';
+newElement.innerHTML = '<h2>Bouquets with a Twist</h2><img src= "img/flowers.jpg" alt="flowers"><p>Like painting with a twist, but with flowers! Grab your friends and join us for a fun night of bouquet building. We will take you through the step by step process of building a bouquet all while enjoying drinks and small bites. We will use seasonal flowers to build a lovely bouquet.</p><p>Call us today to reserve your spot!</>';
 
 selected.appendChild(newElement);
