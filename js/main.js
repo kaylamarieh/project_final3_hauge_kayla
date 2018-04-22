54aaf12e46577af1e56f788464a2299b10263b21
Kayla Hauge <kmhauge@ufl.edu>
2018-04-22 18:47:39 -0400

    add jquery for contact info on mobile

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 $(document).ready(function () {
 
-    $('#phone').click(function () {
+    $('.fa-phone').click(function () {
         $('#add').append("<a href='954-256-7467'>954.256.7467</a>");
+    });
+
+    $('.fa-map-marker').click(function () {
+        $('#add').append('208 SW Sunflower St, Davie, Fl 33345');
+    });
+
+    $('.fa-clock').click(function () {
+        $('#add').append('Open M-F 9am-5pm / S-S 10am-4pm');
+    });
+
+    $('#phone').click(function () {
+        $('#add').append('bloom@flowers.com');
     });
 
 });
