7f355f0941255f072a4429f68d796c5c223576a8
Kayla Hauge <kmhauge@ufl.edu>
2018-04-22 14:57:17 -0400

    add elements to events page with javascript

--- js/main.js
+++ js/main.js
@@ -3,3 +3,10 @@ $(document).ready(function () {
 
 
 });
+
+var selected = document.getElementById('events');
+var newElement = document.createElement('section');
+
+newElement.innerHTML = '<h2>Bouquets with a Twist</h2><img src= "img/flowers.jpg" alt="flowers"><p>Like painting with a twist, but with flowers! Grab your friends and join us for a fun night of bouquet building. We will take you through the step by step process of building a bouquet all while enjoying drinks and small bites. We will use seasonal flowers to build a lovely bouquet.</p>';
+
+selected.appendChild(newElement);
