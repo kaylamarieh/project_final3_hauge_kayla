c6b009a59106e5358e80937919ea4f0f800d73e5
Kayla Hauge <kmhauge@ufl.edu>
2018-04-22 20:28:16 -0400

    add jquery to make icons clickable on mobile

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 https://jquery.com/download/
 http://pixelcog.github.io/parallax.js/
 https://fontawesome.com/
+https://api.jquery.com/empty/
--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 $(document).ready(function () {
 
+    //adds linked clickable phonenumber when phone icon is clicked on mobile
     $('.fa-phone').click(function () {
-        $('#add').append("<a href='954-256-7467'>954.256.7467</a>");
+        $('#add').empty().append('<p><a href="tel:954-256-7467">954.256.7467</a></p>');
     });
 
+    //adds address when map icon is clicked on mobile
     $('.fa-map-marker').click(function () {
-        $('#add').append('208 SW Sunflower St, Davie, Fl 33345');
+        $('#add').empty().append('<p>208 SW Sunflower St, Davie, Fl 33345</p>');
     });
 
+    //adds hours when clock icon is clicked on mobile
     $('.fa-clock').click(function () {
-        $('#add').append('Open M-F 9am-5pm / S-S 10am-4pm');
+        $('#add').empty().append('<p>Open M-F 9am-5pm / S-S 10am-4pm</p>');
     });
 
-    $('#phone').click(function () {
-        $('#add').append('bloom@flowers.com');
+    //adds linked clickable email when envelope icon is clicked on mobile
+    $('.fa-envelope').click(function () {
+        $('#add').empty().append('<p><a href="mailto:bloom@flowers.com">bloom@flowers.com</a></p>');
     });
 
 });
