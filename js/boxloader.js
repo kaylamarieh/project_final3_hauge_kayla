2655025bba733adf253483c90395ac23817ffbfc
Kayla Hauge <kmhauge@ufl.edu>
2018-04-24 19:15:29 -0400

    add the box loader jquery plugin

---
+++ js/boxloader.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    $('.load').boxLoader({
+        direction: 'y',
+        position: '100%',
+        effect: 'fadeIn',
+        duration: '0.5s',
+        windowarea: '50%'
+    });
+});
