3c1896ca178875095b1e3e451c18a66942b468a7
Kayla Hauge <kmhauge@ufl.edu>
2018-04-25 15:40:13 -0400

    add information to events

--- js/events.js
+++ js/events.js
@@ -2,6 +2,6 @@
 var selected = document.getElementById('events');
 var newElement = document.createElement('section');
 
-newElement.innerHTML = '<h2>Bouquets with a Twist</h2><img src= "img/flowers.jpg" alt="flowers"><p>Like painting with a twist, but with flowers! Grab your friends and join us for a fun night of bouquet building. We will take you through the step by step process of building a bouquet all while enjoying drinks and small bites. We will use seasonal flowers to build a lovely bouquet.</p><p>Call us today to reserve your spot!</>';
+newElement.innerHTML = '<h2>Bouquets with a Twist</h2><img src= "img/flowers.jpg" alt="flowers"><p>Like painting with a twist, but with flowers! Grab your friends and join us for a fun night of bouquet building. We will take you through the step by step process of building a bouquet all while enjoying drinks and small bites. We will use seasonal flowers to build a lovely bouquet.</p><p>Thursday, May 3, 2018</p><p>7pm-11pm</p><p>Call us today to reserve your spot!</>';
 
 selected.appendChild(newElement);
